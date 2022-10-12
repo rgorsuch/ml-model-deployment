afcb0b397654b454116e3bfe4e1696667892d70c
Ryan Gorsuch <ryan.gorsuch@gmail.com>
2022-10-12 20:43:51 +0000

    Added classifier for GCP.

---
+++ gcp-classifier.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 11:29:19 2022
+
+@author: ryan.gorsuch
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 11:18:11 2022
+
+@author: ryan.gorsuch
+"""
+import numpy as np
+import pickle
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+local_classifier = pickle.load(open("classifier.pickle", "rb"))
+local_scaler = pickle.load(open("sc.pickle", "rb"))
+
+@app.route("/model", methods=["POST"])
+def hello_model():
+    request_data = request.get_json(force=True)
+    #model_name = request_data["model"]
+    age = request_data["age"]
+    salary = request_data["salary"]
+    prediction = local_classifier.predict(
+        local_scaler.transform(
+            np.array([[age, salary]])
+        )
+    )
+    probability = local_classifier.predict_proba(
+        local_scaler.transform(
+            np.array([[age, salary]])
+        )
+    )[:,1]
+
+    return f"The GCP API prediction is {prediction} or {probability}\n"
+
+if __name__ == "__main__":
+    #app.run(port=8000, debug=True)
+    app.run(host="0.0.0.0", port=80)
+
